062a9555d67dd493ba82f440062745c0517bdfa7
denebtech <techdeneb@gmail.com>
2019-03-11 03:59:02 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,5 @@
+import app from './src/app';
+
+const { PORT = 8080, NODE_ENV = 'development' } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}, environment: ${NODE_ENV}`, )); // eslint-disable-line no-console
